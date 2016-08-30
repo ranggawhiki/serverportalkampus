3306ae69846dfc8824ca9261d9036a2bd58ad257
Rangga Whiki Pangestu <whikipangestu@gmail.com>
2016-08-30 16:12:04 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,7 +3,7 @@
 require_once('fungsi_validasi.php');
 
 // definisikan koneksi ke database
-$server = "localhost";
+$server = "pma.passionit.net/localhost";
 $username = "notif";
 $password = "JangkrikBos";
 $database = "notif";
